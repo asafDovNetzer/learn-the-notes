23bdf0f5e66c268dffc9d3690893628761821598
asafDovNetzer <asaf.ntzr@gmail.com>
2021-05-24 16:39:45 +0300

    s

--- src/Components/Options/Options.js
+++ src/Components/Options/Options.js
@@ -5,19 +5,37 @@ import classes from "./Options.module.css";
 const options = (props) => {
   const optionsArray = [];
 
-  props.options.forEach((option, index) => {
-    optionsArray.push(
-      <Option
-        index={index}
-        key={option.name}
-        disabled={option.isDisabled}
-        symbolType={props.symbolType}
-        note={option.name}
-        symbol={option.symbol}
-        handler={(Option) => props.handler(Option)}
-      />
-    );
-  });
+  if (window.outerWidth < 500) {
+    props.options.forEach((option, index) => {
+      optionsArray.unshift(
+        <Option
+          index={index}
+          key={option.name}
+          disabled={option.isDisabled}
+          symbolType={props.symbolType}
+          note={option.name}
+          symbol={option.symbol}
+          handler={(Option) => props.handler(Option)}
+        />
+      );
+    });
+  }
+
+  if (window.outerWidth >= 500) {
+    props.options.forEach((option, index) => {
+      optionsArray.push(
+        <Option
+          index={index}
+          key={option.name}
+          disabled={option.isDisabled}
+          symbolType={props.symbolType}
+          note={option.name}
+          symbol={option.symbol}
+          handler={(Option) => props.handler(Option)}
+        />
+      );
+    });
+  }
 
   const className = [classes.Options, props.isRunning ? null : classes.Hidden];
 
