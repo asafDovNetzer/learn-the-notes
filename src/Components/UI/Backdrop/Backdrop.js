ca90692c161db2f6d1c465346764ff5fe929a95d
asafDovNetzer <78555033+asafDovNetzer@users.noreply.github.com>
2021-05-24 15:15:44 +0300

    Delete src directory

--- src/Components/UI/Backdrop/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import classes from "./Backdrop.module.css";
-
-const backdrop = (props) =>
-  props.show ? (
-    <div onClick={props.closeModal} className={classes.Backdrop}></div>
-  ) : null;
-
-export default backdrop;
